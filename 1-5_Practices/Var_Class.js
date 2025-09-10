d5f88e08a93e9193b1a38be0e443a4756dc88c8e
Thegonzzo <camaleon199@hotmail.com>
2025-09-09 21:30:13 -0600

    Var Class added

---
+++ 1-5_Practices/Var_Class.js
@@ -0,0 +1,12 @@
+let andyBox = 'Woody'
+console.log(andyBox);
+
+// INcorrect way
+let c = 'Woody'
+let ab = 'Woody'
+let andyB = 'Woody'
+
+// Correct way
+let andyFirstToy = 'Woody'
+let userLink = 'https://www.google.com'
+let userID = '12345678'
